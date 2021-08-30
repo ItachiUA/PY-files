07e14dc348e3c99f650d375e5a663297598279a2
ItachiUchiha <87782833+ItachiUA@users.noreply.github.com>
2021-08-30 23:42:10 +0300

    Update pic-py-bot.py

--- pic-py-bot.py
+++ pic-py-bot.py
@@ -2,5 +2,5 @@ import telebot
 token = '1948450134:AAGAmwYNHc838aclNjtTA1t1DWq6554ppfU'
 bot = telebot.TeleBot(token)
 def repeat_all_messages():
-    bot.send_message(523442794, 'hello world!')
+    bot.send_message('-523442794', 'hello world!')
 repeat_all_messages()
